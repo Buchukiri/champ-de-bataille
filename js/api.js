b68abe8b3cf0205f1724c8d56ca6e3dbd7cab362
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-06-23 16:08:13 +0200

    js fighters imagesXs name

--- js/api.js
+++ js/api.js
@@ -2,14 +2,28 @@
 async function waitingForResponse() {
 const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json")
 const fighterList = await response.json();
-
-fighterList.forEach(fighter => {
-    const namesFighters = fighter.name;
-    const imgXsFighters = fighter.images.xs;
-    console.log(fighter.images.xs);
-});
+getNamesFighters(fighterList);
+// getImagesXsFighters(fighterList);
 }
 waitingForResponse()
+
+
+// Names
+function getNamesFighters(array){
+    array.forEach(fighter => {
+        const namesFighters = fighter.name;
+        console.log(namesFighters);
+    });
+}
+
+
+// ImagesXs
+// function getImagesXsFighters(array){
+//     array.forEach(fighter => {
+//         const imgXsFighters = fighter.images.xs;
+//         console.log(imgXsFighters);
+//     });
+// }
 
 
 
