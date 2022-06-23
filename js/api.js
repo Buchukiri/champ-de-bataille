c6f751c6a9e83fdc29a711e80affaf0125663bcf
Baimenov Timour <buchukiri@gmail.com>
2022-06-23 15:02:31 +0200

    add 2nd js in html

--- js/api.js
+++ js/api.js
@@ -1,12 +1,4 @@
 
-
-
-
-
-
-
-
-
 async function waitingForResponse() {
 const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json")
 const fighterList = await response.json();
@@ -16,5 +8,7 @@ fightersArray.forEach(fighter => {
 });
 }
 waitingForResponse()
+
+
 
 
