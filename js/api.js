dc4d28b5702a1f5f47404d288347ae0ce5009256
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-06-23 15:42:38 +0200

    création html / css

--- js/api.js
+++ js/api.js
@@ -2,11 +2,9 @@
 async function waitingForResponse() {
 const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json")
 const fighterList = await response.json();
-// const fightersArray = Object.values(fighterList);
-console.log(fighterList);
 
 fighterList.forEach(fighter => {
-    // console.log(fighter.name);
+    console.log(fighter.name);
 });
 }
 waitingForResponse()
